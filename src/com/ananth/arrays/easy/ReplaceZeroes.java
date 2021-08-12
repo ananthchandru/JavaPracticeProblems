5f79b61fb4057dd050c259638019ee9afe996195
Anantha Subramanian <ananths1006@google.com>
2021-08-12 15:30:33 +0530

    ReplaceZero

---
+++ src/com/ananth/arrays/easy/ReplaceZeroes.java
@@ -0,0 +1,65 @@
+package com.ananth.arrays.easy;
+
+class ReplaceZeroes {
+
+    public static void main(String[] args) {
+        int input = -10;
+        int replaceWith = 5;
+        ReplaceZeroes obj = new ReplaceZeroes();
+        int output = obj.replaceZeroes(input, replaceWith);
+        System.out.println(output);
+    }
+
+    public int replaceZeroes(int input, int replaceWith) {
+
+        if(input == 0)
+            return replaceWith;
+
+        boolean isNegative = false;
+
+        if(input < 0) {
+            isNegative = true;
+            input = input * -1;
+        }
+
+        int output = 0;
+        int placeValue = 1;
+
+        while(input > 0) {
+            int modulus = input % 10;
+            if(modulus == 0)
+                output += replaceWith * placeValue;
+            else {
+                output += modulus * placeValue;
+            }
+            input = input/10;
+            placeValue = placeValue * 10;
+        }
+        return (isNegative ? -1 * output : output);
+    }
+}
+
+/*
+        Special cases:
+        i) Input is zero
+        i) negative numbers
+        ii) Floating numbers
+
+Run
+        1003 => 	output = 0, placeValue = 1
+        while(1003 >0)
+        modulus = 3; output = 0 + 3*1 =>
+        output= 3; input  = 100; placeValue = 10;
+
+        while(100 >0)
+        modulus = 0; output = 3 + (5*10) =>
+        output = 53; input = 10; placeValue = 100;
+
+        while(10 > 0)
+        modulus = 0; output = 53 + (5*100) =>
+        output = 553; input = 1; placeValue = 1000;
+
+        while(1 > 0)
+        modulus = 1; output = 553 + (1*1000) =>
+        output = 1553; input = 0; placeValue = 10000;
+	*/
---
+++ src/com/ananth/arrays/easy/ReplaceZeroesRecursion.java
@@ -0,0 +1,50 @@
+package com.ananth.arrays.easy;
+
+class ReplaceZeroesRecursion {
+
+    int replaceWith = 5;
+
+    public static void main(String[] args) {
+        int input = 100;
+        ReplaceZeroesRecursion obj = new ReplaceZeroesRecursion();
+        int output = obj.replaceZeroes(input);
+        System.out.println(output);
+    }
+
+    public int replaceZeroes(int input) {
+        if(input == 0)
+            return replaceWith;
+        if(input < 0)
+            return -1 * replaceZeroesRecursive(input * -1);
+		else
+            return replaceZeroesRecursive(input);
+    }
+
+    public int replaceZeroesRecursive(int input) {
+        if(input == 0)
+            return 0;
+
+        int modulusSum = input % 10;
+
+        if(modulusSum == 0)
+            modulusSum = replaceWith;
+
+        return replaceZeroesRecursive(input/10)*10 + modulusSum;
+    }
+}
+
+/*
+        Special cases:
+        i) Input is zero
+        i) negative numbers
+        ii) Floating numbers
+
+        Recursion
+        i) Stop condition - return 0 when input becomes 0
+        ii) Function will return modulus (input % 10)
+            (if modulus is 0, return 5)
+        iii) function output * 10 for place value handling during each recursive call
+        iv) Add modulus to function output
+
+
+ */
