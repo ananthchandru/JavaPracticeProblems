829afba91a9f51a78c8d7655763ca5b0e5caf9a8
Anantha Subramanian <ananths1006@google.com>
2022-04-13 11:14:28 +0530

    Minimum distance

--- src/com/ananth/arrays/easy/MinimumDistance.java
+++ src/com/ananth/arrays/easy/MinimumDistance.java
@@ -3,46 +3,42 @@ package com.ananth.arrays.easy;
 public class MinimumDistance {
 
     public static void main(String[] args) {
-        int[] arr = {0, 5, 6, 1, 4, 5, 5, 4, 6, 7};
+        int[] arr = {0, 5, 8, 1, 6, 7, 5};
         System.out.println(minDist1(arr, arr.length, 5, 6));
 //        int[] arr = {1, 2, 3, 2};
 //        System.out.println(minDist1(arr, arr.length, 1, 4));
     }
-    //Longer code O(n)
-    static int minDist(int arr[], int n, int x, int y) {
-        int sub = -1, min_x = -1, min_y = -1;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x)
-                min_x = i;
-            else if (arr[i] == y)
-                min_y = i;
 
-            if (min_x != -1 && min_y != -1) {
-                if (sub == -1)
-                    sub = min_y - min_x;
-                else
-                    Math.min(sub, (min_y - min_x));
+    //Method 1: O(N^2) - two loops
+
+    static int minDist(int arr[], int n, int x, int y) {
+        int min_distance = Integer.MAX_VALUE;
+        for(int i=0;i<n-1;i++) {
+            for(int j=i+1;j<n;j++) {
+                if((arr[i]==x && arr[j]==y) || (arr[j]==x && arr[i]==y)
+                        && min_distance > Math.abs(i-j)) {
+                    min_distance = Math.abs(i-j);
+                }
             }
         }
-        return sub;
+        return min_distance;
     }
+
     //Shorter code O(n)
     static int minDist1(int arr[], int n, int x, int y) {
-
         //previous index and min distance
-        int i = 0, p = -1, min_dist = Integer.MAX_VALUE;
+        int prev = -1, min_dist = Integer.MAX_VALUE;
 
-        for (i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             if (arr[i] == x || arr[i] == y) {
                 //we will check if p is not equal to -1 and
                 //If the element at current index matches with
                 //the element at index p , If yes then update
                 //the minimum distance if needed
-                if (p != -1 && arr[i] != arr[p])
-                    min_dist = Math.min(min_dist, i - p);
-
+                if (prev != -1 && arr[i] != arr[prev])
+                    min_dist = Math.min(min_dist, i - prev);
                 //update the previous index
-                p = i;
+                prev = i;
             }
         }
         //If distance is equal to int max
