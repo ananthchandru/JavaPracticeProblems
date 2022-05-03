ce94a258f89a14cee947ae1c98343bc8e575f45a
Anantha Subramanian <ananths1006@google.com>
2022-05-03 23:56:41 +0530

    MissingNumberArray

--- src/com/ananth/arrays/easy/MissingNumberArray.java
+++ src/com/ananth/arrays/easy/MissingNumberArray.java
@@ -3,14 +3,15 @@ package com.ananth.arrays.easy;
 public class MissingNumberArray {
 	 
     public static void main(String[] args) {
-        int[] arr1={7,5,8,6,4,2};
-        System.out.println("Missing number from array arr1: "+missingNumber(arr1));
+        int[] arr1={7,5,8,6,4,2,3};
+        //passing (arr.length+1) as it is the original size including missing number
+        System.out.println("Missing number from array arr1: "+missingNumber(arr1, arr1.length+1));
         int[] arr2={5,3,1,2};
-        System.out.println("Missing number from array arr2: "+missingNumber(arr2));
+        //passing (arr.length+1) as it is the original size including missing number
+        System.out.println("Missing number from array arr2: "+missingNumber(arr2, arr2.length+1));
     }
  
-    public static int missingNumber(int[] arr) {
-        int n=arr.length+1;
+    public static int missingNumber(int[] arr, int n) {
         int sum=n*(n+1)/2;
         int restSum=0;
         for (int i = 0; i < arr.length; i++) {
