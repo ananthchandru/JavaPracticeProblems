b4b2c08eda41311ba332869e69c8bbdeb290d7fb
Anantha Subramanian <ananths1006@google.com>
2022-05-03 23:42:44 +0530

    Repetitiveaddofdigits

--- src/com/ananth/arrays/easy/RepetitiveAdditionOfDigits.java
+++ src/com/ananth/arrays/easy/RepetitiveAdditionOfDigits.java
@@ -3,8 +3,9 @@ package com.ananth.arrays.easy;
 public class RepetitiveAdditionOfDigits {
 
     public static void main(String[] args) {
-        int num = 8888;
+        int num = 98949834;
         System.out.println(getSumOfDigits(num));
+        System.out.println(singleDigit1(num));
     }
 
     public static int getSumOfDigits(int num) {
@@ -18,5 +19,24 @@ public class RepetitiveAdditionOfDigits {
             num = sum;
         }
         return num;
+    }
+
+    //Formula
+
+    static int singleDigit1(int N){
+        return 1+(N-1)%9;
+    }
+
+    // Recursion
+    static int singleDigit2(long N){
+        int digit = 0;
+        while(N>0) {
+            digit += N%10;
+            N/=10;
+        }
+        if(digit>9) {
+            return singleDigit1(digit);
+        }
+        return digit;
     }
 }
